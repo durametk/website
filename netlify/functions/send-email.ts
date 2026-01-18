8fcf9a18d56d6305c7cd95d1286b6624eef454a0
pvgtest <aniketcoolshe@gmail.com>
2026-01-18 20:51:19 +0530

    Update changes

--- netlify/functions/send-email.ts
+++ netlify/functions/send-email.ts
@@ -2,7 +2,7 @@
 import { Resend } from "resend";
 import type { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
 
-const resend = new Resend(process.env.RESEND_API_KEY || "re_2dz3tHHJ_HGwXiSLcCfR924BFyk8TqFit");
+const resend = new Resend(process.env.RESEND_API_KEY || "re_Wz5buHio_4diMFr7nDkwBRSGfy31kqi4b
 
 export const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
   // Handle CORS
